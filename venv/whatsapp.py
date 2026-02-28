2467814537765be2b0a25114ada136d8fcca0792
ceazer <ww.mohammed.mn@gmail.com>
2026-02-28 14:18:53 +0300

    Final cleaning

--- venv/whatsapp.py
+++
@@ -1,7 +0,0 @@
-import pywhatkit as pwk
-pwk.sendwhatmsg_instantly(
-    phone_no="+79254101873",  # Include country code
-    message="Hello from Python!",
-    wait_time=15,  # Seconds to wait before sending
-    tab_close=True  # Close browser tab after sending
-)
